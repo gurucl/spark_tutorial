724a5077b07232b959870fa789ec20281af5178a
Gurumurthy C L <gurumurthy.cl@optum.com>
2020-03-01 21:29:19 +0530

    Adding Pavan Hive JSON TimeStamp Format solution - 01 Mar 2020

---
+++ src/main/scala/com/mycompany/myproject/core/Pavan_Hive_Json.scala
@@ -0,0 +1,83 @@
+package com.mycompany.myproject.core
+
+import java.text.SimpleDateFormat
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
+object Pavan_Hive_Json {
+
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession.builder().appName("Pavan_TimeStamp_Analysis").master("local[1]").getOrCreate()
+
+    val sc = spark.sparkContext
+
+    sc.setLogLevel("ERROR")
+
+    import spark.implicits._
+
+    val tsUDF = udf(convertToHiveTs:(String)=>(String))
+
+    val hiveJsonDF = spark.read.json("/Users/gcl/Documents/Github/spark_tutorial/src/main/resources/Assignments/hive.json")
+
+    hiveJsonDF.printSchema()
+
+    hiveJsonDF.show(false)
+
+    val outDF = hiveJsonDF.withColumn("startTime", tsUDF(col("startTime")))
+      .withColumn("endTime", tsUDF(col("endTime")))
+      .withColumn("InitialStartTime", tsUDF(col("InitialStartTime")))
+
+    outDF.show(false)
+
+
+  }
+
+  def convertToHiveTs(input:String):String = {
+
+    val date = new SimpleDateFormat("yyyyMMdd'T'HHmmss").parse(input)
+
+    val output = new SimpleDateFormat("yyyyMMdd HHmmss").format(date)
+
+    //println(output)
+
+    output
+
+  }
+
+}
+
+
+/*
+
+====== INPUT : hive.json =======
+{"endTime": "20190220T084445","GUID": "adhgihdb-jkbdfhwe-knkhdw", "startTime": "20190211T104916", "InitialStartTime": "20181117T011141"}
+{"endTime": "20190220T084445","GUID": "adhgihdb-jkbdfhwe-knkhdw", "startTime": "20190211T104916", "InitialStartTime": "20181117T011141"}
+{"endTime": "20190220T084445","GUID": "adhgihdb-jkbdfhwe-knkhdw", "startTime": "20190211T104916", "InitialStartTime": "20181117T011141"}
+
+====== OUTPUT =======
+
+root
+ |-- GUID: string (nullable = true)
+ |-- InitialStartTime: string (nullable = true)
+ |-- endTime: string (nullable = true)
+ |-- startTime: string (nullable = true)
+
++------------------------+----------------+---------------+---------------+
+|GUID                    |InitialStartTime|endTime        |startTime      |
++------------------------+----------------+---------------+---------------+
+|adhgihdb-jkbdfhwe-knkhdw|20181117T011141 |20190220T084445|20190211T104916|
+|adhgihdb-jkbdfhwe-knkhdw|20181117T011141 |20190220T084445|20190211T104916|
+|adhgihdb-jkbdfhwe-knkhdw|20181117T011141 |20190220T084445|20190211T104916|
++------------------------+----------------+---------------+---------------+
+
++------------------------+----------------+---------------+---------------+
+|GUID                    |InitialStartTime|endTime        |startTime      |
++------------------------+----------------+---------------+---------------+
+|adhgihdb-jkbdfhwe-knkhdw|20181117 011141 |20190220 084445|20190211 104916|
+|adhgihdb-jkbdfhwe-knkhdw|20181117 011141 |20190220 084445|20190211 104916|
+|adhgihdb-jkbdfhwe-knkhdw|20181117 011141 |20190220 084445|20190211 104916|
++------------------------+----------------+---------------+---------------+
+
+ */
